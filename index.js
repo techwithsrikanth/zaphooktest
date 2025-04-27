ca1418e07e09cf2d529bc1df9346e233a2527dcb
techwithsrikanth <srikanthprakash072003@gmail.com>
2025-04-27 16:18:40 +0530

    V16

--- index.js
+++ index.js
@@ -17,8 +17,18 @@ app.use(express.json());
 const sheets = google.sheets('v4');
 
 // Load service account credentials
-const credentials = JSON.parse(fs.readFileSync(path.join(__dirname, 'gen-lang-client-0144790529-6e79e16b7e7a.json')));
-
+const credentials = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Handling newline character in private key
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+};
 // Authenticate with Google API
 const auth = new google.auth.GoogleAuth({
   credentials: credentials,
